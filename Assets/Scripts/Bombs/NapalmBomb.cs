202ab6ce63962f129ba47a0607cee863ea9a7bf4
fran <fran.androic@fer.hr>
2026-07-25 23:21:58 +0200

    working on enemy ships

--- Assets/Scripts/Bombs/NapalmBomb.cs
+++ Assets/Scripts/Bombs/NapalmBomb.cs
@@ -6,10 +6,11 @@ public class NapalmBomb : Bomb
     [SerializeField] private float napalmFieldLifetime;
     [SerializeField] private Vector3 napalmFieldScale;
 
-    protected override void preExplosionLogic()
+    protected override void explosionLogic(IHittable hittable, bool isSelfCaused)
     {
         GameObject napalmField = Instantiate(napalmFieldPrefab, gameObject.transform.position, Quaternion.identity);
         napalmField.transform.localScale = napalmFieldScale;
         Destroy(napalmField, napalmFieldLifetime);
+        hittable.Hit(damage);
     }
 }
