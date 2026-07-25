6a0e6747baa08c431f6677a44bc61892ccfe732d
fran <fran.androic@fer.hr>
2026-07-25 23:59:41 +0200

    imported jedrilica

--- Assets/Scripts/BombDropper.cs
+++ Assets/Scripts/BombDropper.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class BombDropper : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject bombPrefab;
+    [SerializeField] private int dropIntervalInTicks;
+
+    private int ticksPassed;
+
+    private void Start()
     {
-        
+        ticksPassed = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        TickDriver.instance.OnTick += Tick;
+    }
+
+    private void OnDisable()
+    {
+        TickDriver.instance.OnTick -= Tick;
+    }
+
+    void Tick()
+    {
+        ticksPassed++;
+
+        if (ticksPassed >= dropIntervalInTicks)
+        {
+            Instantiate(bombPrefab, gameObject.transform.position, Quaternion.identity);
+            ticksPassed = 0;
+        }
     }
 }
--- Assets/Scripts/BombStuff/BombSpawner.cs
+++ Assets/Scripts/BombStuff/BombSpawner.cs
@@ -2,21 +2,34 @@
 
 public class BombSpawner : MonoBehaviour
 {
-    private float totalTimer = 0;
     [SerializeField] private Bomb[] bombs;
     [SerializeField] private float[] weightedProbabilities;
-    private float timeUntilNextBomb = 2;
-    private void Update()
+
+    private int totalTimer = 0;
+    private int timeUntilNextBomb = 2;
+
+    private void OnEnable()
     {
-        totalTimer += Time.deltaTime;
-        timeUntilNextBomb -= Time.deltaTime;
+        TickDriver.instance.OnTick += Tick;
+    }
+
+    private void OnDisable()
+    {
+        TickDriver.instance.OnTick -= Tick;
+    }
+
+    void Tick()
+    {
+        totalTimer++;
+        timeUntilNextBomb--;
+
         if (timeUntilNextBomb <= 0)
         {
             Vector2 location = new Vector2(Random.Range(-7f, 7f), Random.Range(-3.5f, 3.5f)) * Scaler.Scale;
             Bomb bombType = MathHelper.WeightedRandomFromDistributionArray<Bomb>(bombs, weightedProbabilities);
             Instantiate(bombType, location.xoy(), Quaternion.identity);
-            
-            timeUntilNextBomb = Random.Range(1f, 1f + Mathf.Min(6, 60 / totalTimer));
+
+            timeUntilNextBomb = Random.Range(1, 1 + Mathf.Min(6, 60 / totalTimer));
         }
     }
 }
