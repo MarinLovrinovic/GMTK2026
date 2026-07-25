c8c36db9a6d753190eb4b04ce77470a3be5e30dc
fran <fran.androic@fer.hr>
2026-07-25 16:39:10 +0200

    added napalm bomb and related assets

---
+++ Assets/Scripts/BombStuff/Explosions/NapalmExplosion.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class NapalmExplosion : Explosion
+{
+    protected override void doEffects()
+    {
+        base.doEffects();
+    }
+}
---
+++ Assets/Scripts/Bombs/NapalmBomb.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class NapalmBomb : Bomb
+{
+    [SerializeField] private GameObject napalmFieldPrefab;
+    [SerializeField] private float napalmFieldLifetime;
+    [SerializeField] private Vector3 napalmFieldScale;
+
+    protected override void preExplosionLogic()
+    {
+        GameObject napalmField = Instantiate(napalmFieldPrefab, gameObject.transform.position, Quaternion.identity);
+        napalmField.transform.localScale = napalmFieldScale;
+        Destroy(napalmField, napalmFieldLifetime);
+    }
+}
