aa7042330bcc15150899f78b63973f99f3eb3cf2
fran <fran.androic@fer.hr>
2026-07-25 19:47:10 +0200

    minor changes to prefabs

--- Assets/Scripts/Bombs/FreezeBomb.cs
+++ Assets/Scripts/Bombs/FreezeBomb.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 public class FreezeBomb : Bomb
 {
     [SerializeField] private float freezeTime;
+    [SerializeField] private GameObject icePrefab;
+
+    private Vector3 localOffset = new Vector3(0.0f, 0.4f, 0.1f);
 
     protected override void explosionLogic(IHittable hittable, bool isSelfCaused)
     {
         hittable.Freeze(freezeTime);
+        
+        if (hittable.GetType() == typeof(Vehicle))
+        {
+            GameObject iceCube = Instantiate(icePrefab, ((Vehicle)hittable).gameObject.transform);
+            iceCube.transform.localPosition = localOffset;
+            Destroy(iceCube, freezeTime);
+        }
     }
 }
