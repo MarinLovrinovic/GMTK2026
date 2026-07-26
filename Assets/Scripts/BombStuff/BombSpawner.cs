0c724fe692c7e50e41ce45a3d048d09d9f425a8e
MarinLovrinovic <lovrinovic.marin@gmail.com>
2026-07-26 19:52:41 +0200

    fixes

--- Assets/Scripts/BombStuff/BombSpawner.cs
+++ Assets/Scripts/BombStuff/BombSpawner.cs
@@ -37,7 +37,7 @@ public class BombSpawner : MonoBehaviour
 
     private void SpawnBomb()
     {
-        Vector2 location = new Vector2(Random.Range(-12f, 12f), Random.Range(-2f, 9f)) * Scaler.Scale;
+        Vector2 location = new Vector2(Random.Range(-12f, 12f), Random.Range(-2f, 9f));
         
         // 40 attempts to find a location that does not overlap with other bombs,
         // otherwise cancel spawn
